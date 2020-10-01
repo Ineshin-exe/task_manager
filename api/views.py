2510703f2359e9a86dcf2935165332d4d4ade7a5
Nikita Ineshin <ineshin.no@gmail.com>
2020-10-01 16:21:41 +0300

    New: - "status" for Responses. - Check illegal filters for "/api/list-task/"

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .serializers import TaskSerializer
@@ -6,45 +8,53 @@ from .models import Task
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def taskList(request):
     list = Task.objects.filter(owner=request.user)
+    params = request.query_params
 
-    status = request.query_params.get('status', None)
-    deadline = request.query_params.get('deadline', None)
-    if status is not None:
-        list = list.filter(status=status)
-    if deadline is not None:
-        list = list.filter(deadline=deadline)
+    for filter in params.dict():
+        if filter == 'status':
+            list = list.filter(status=params[filter])
+        elif filter == 'deadline':
+            list = list.filter(deadline=params[filter])
+        else:
+            return Response({'message': 'filter \'{}\' is not supported'.format(filter)}, status=status.HTTP_400_BAD_REQUEST)
 
     serializer = TaskSerializer(list, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def taskCreate(request):
 
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save(owner=request.user)
-        return Response(serializer.data)
-    return Response(serializer.errors)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def taskItem(request, pk):
-    task = Task.objects.get(id=pk)
+    try:
+        task = Task.objects.get(id=pk)
+    except:
+        return Response({'message': 'Task does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = TaskSerializer(task)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == 'POST':
+    if request.method == 'PATCH':
         serializer = TaskSerializer(instance=task, data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         task.delete()
-        return Response({'message': 'Task was deleted successfully!'})
+        return Response({'message': 'Task was deleted successfully!'}, status=status.HTTP_200_OK)
